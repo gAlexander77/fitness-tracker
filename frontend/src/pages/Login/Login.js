4b0f3f9b7e4287fe0e6dc3247944ad2afc1f6aeb
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-02-22 14:06:41 -0600

    Update Login and Signup page

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from "react-router-dom";
+import TextField from '@mui/material/TextField';
 
 function Login(){
+
+    const [input, setInput] = useState({
+        username: '',
+        password: ''
+    })
+
+    const inputHandler = (evt) => {
+        setInput({
+            ...input,
+            [evt.target.id]: evt.target.value
+        });
+        console.log(input)
+    }
+
     return(
         <div className="login-page">
-            Login
+            <div className="login-inner">
+                <h1>Login</h1>
+                <div className="login-input-container">
+                    <TextField
+                        id = 'username'
+                        label="Username"
+                        type="text"
+                        value={input.username}
+                        onChange={inputHandler}   
+                    />  
+                    <TextField
+                        id = 'password'
+                        label="Password"
+                        type="password"
+                        value={input.password}
+                        onChange={inputHandler}
+                    />
+                </div>
+                <button>Login</button>
+                <Link to="/signup">
+                    <button>Create a account</button>
+                </Link>
+            </div>
         </div>
     );
 }
--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import TextField from '@mui/material/TextField';
 
 function SignUp(){
+    
+    const [input, setInput] = useState({
+        username: '',
+        password: '',
+        verifyPassword: ''
+    })
+
+    const inputHandler = (evt) => {
+        setInput({
+            ...input,
+            [evt.target.id]: evt.target.value
+        });
+        console.log(input)
+    }
+
     return(
         <div className="sign-up-page">
-            SignUp
+            <h1>Sign Up</h1>
+            <div className="sign-up-inner">
+                <TextField
+                    id="username"
+                    label="Username"
+                    type="text"
+                    value={input.username}
+                    onChange={inputHandler}
+                />
+                <TextField
+                    id="password"
+                    label="Password"
+                    type="password"
+                    value={input.password}
+                    onChange={inputHandler}
+                />
+                <TextField
+                    id="verifyPassword"
+                    label="Verify Password"
+                    type="password"
+                    value={input.verifyPassword}
+                    onChange={inputHandler}
+                />    
+            </div>
+            <button>Sign Up</button>
+            <Link to="/login">
+                <button>Already have an account</button>
+            </Link>
         </div>
     );
 }
