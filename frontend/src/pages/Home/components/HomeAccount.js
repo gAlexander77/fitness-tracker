627a97022e7aa43a1e2125a8249465a06d790dad
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-02-23 01:42:01 -0600

    Add user dashboard link to home

--- frontend/src/pages/Home/components/HomeAccount.js
+++ frontend/src/pages/Home/components/HomeAccount.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { FaUserCircle } from 'react-icons/fa';
+import '../../../styles/pages/Home/Home.css';
 
 function HomeAccount(props) {
     return(props.isUser) ? (
-        <div className="home-home-account-container">
-            MY ACCOUNT
+        <div className="home-account-container">
+            <Link to='/dashboard'>
+                <button>
+                    <p>Dashboard</p>
+                    <FaUserCircle id="icon"/>
+                </button>
+            </Link>
         </div>
     ) : '';
 }
