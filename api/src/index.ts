b7801c1663f44348140b97902a5e507f57e46b8f
BarkerProoks <jonparkerbrooks@gmail.com>
2023-02-23 01:20:13 -0600

    Added support for environment variables

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,10 +7,14 @@ import { connectToDatabase } from './db';
 import users from './routes/users';
 import auth from './routes/auth';
 
+const HOST = process.env.SS_API_HOST || "127.0.0.1";
+const PORT = parseInt(process.env.SS_API_PORT) || 3000;
+
 // first thing's first, connect to the database
 // source: db.ts
-connectToDatabase("testing") // specify database
-	.then((_db: Db) => {
+connectToDatabase(process.env.SS_API_DB || "testing") // specify database
+	.then((db: Db) => {
+
 		// MAIN: API SERVER
 		const app = express(); // grab a handle to the main express object
 		const api = [ // we've defined this array of objects here to make the code more
@@ -18,6 +22,7 @@ connectToDatabase("testing") // specify database
 			{ path: "/users", router: users }, // more manageable
 			{ path: "/public", router: express.static("./public") }
 		];
+		
 		app.use(express.json()); // this is a JSON API
 		app.use(session({ // set up sessions for user authentication
 			secret: "fitness",
@@ -25,10 +30,16 @@ connectToDatabase("testing") // specify database
 			saveUninitialized: true,
 			cookie: {} // in release this should be { secure = true }
 		}));
+
 		// setup the routes we defined above
 		api.forEach(({path, router}) => app.use(path, router));
-		app.listen(3000, "127.0.0.1", () => { // start the server, the callback function is 
-			console.log("listening on localhost:3000"); // just going to show us the server's running
+		app.listen(PORT, HOST, () => { // start the server, the callback function is 
+			console.log("ShapeShift API v1");
+			console.log("-----------------");
+			console.log(`  + SS_API_HOST: ${HOST}`);
+			console.log(`  + SS_API_PORT: ${PORT}`);
+			console.log(`  + SS_API_DB: ${db.databaseName}`);
+			console.log(`- url: http://${HOST}:${PORT}`); // just going to show us the server's running
 		});
 	}) // if something gets mangled, we'll arive here
 	.catch((error: Error) => console.log(error));
