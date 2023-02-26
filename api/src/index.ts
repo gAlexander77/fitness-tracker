2af8d706cdacde00fb0f221e5d4520962b6c1131
BarkerProoks <jonparkerbrooks@gmail.com>
2023-02-25 22:29:00 -0600

    Added CORS support

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 // NPM imports
 import session from 'express-session';
-import express from 'express';
+import express, { Express, RequestHandler, Router } from 'express';
+import cors from 'cors';
 import { Db } from 'mongodb';
 // local imports
 import { connectToDatabase } from './db';
@@ -8,18 +9,16 @@ import users from './routes/users';
 import auth from './routes/auth';
 
 const ENV = process.env;
-const HOST = ENV.API_HOST || "127.0.0.1";
+const HOST = ENV.API_HOST || "localhost";
 const PORT = parseInt(ENV.API_PORT) || 3000;
 const STATIC_DIR = ENV.API_STATIC_DIR || "./public";
 
 export const DB = ENV.API_DB || "testing";
-export const DB_HOST = ENV.API_DB_HOST || "127.0.0.1";
+export const DB_HOST = ENV.API_DB_HOST || "localhost";
 export const DB_PORT = parseInt(ENV.API_DB_PORT) || 27017;
 export const DB_USERNAME = ENV.API_DB_USERNAME || "testing";
 export const DB_PASSWORD = ENV.API_DB_PASSWORD || "testing";
 
-// first thing's first, connect to the database
-// source: db.ts
 connectToDatabase(DB, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD) // specify database
 	.then((db: Db) => {
 
@@ -27,12 +26,16 @@ connectToDatabase(DB, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD) // specify dat
 		const app = express(); // grab a handle to the main express object
 		const api = express.Router(); // create the API route
 		
-		[ // we've defined this array of objects here to make the code more
-			{ path: "/", router: auth }, // readable, and to make extending the API
-			{ path: "/users", router: users }, // more manageable
+		// apply routes to api
+		[{ path: "/", router: auth },
+		 { path: "/users", router: users }
 		].forEach(({path, router}) => api.use(path, router));
-
+		
 		app.use(express.json());
+		app.use(cors({
+			origin: "http://localhost:3000",
+			credentials: true
+		}));
 		app.use(session({ // set up sessions for user authentication
 			secret: "fitness",
 			resave: false,
@@ -40,9 +43,9 @@ connectToDatabase(DB, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD) // specify dat
 			cookie: {} // in release this should be { secure = true }
 		}));
 		
-		[ // set up the app routes
-			{ path: "/api", router: api },
-			{ path: "/public", router: express.static(STATIC_DIR) },
+		// apply routes to app
+		[{ path: "/api", router: api },
+		 { path: "/public", router: express.static(STATIC_DIR) },
 		].forEach(({path, router}) => app.use(path, router))
 		
 		app.listen(PORT, HOST, () => { // start the server, the callback function is 
@@ -54,12 +57,12 @@ connectToDatabase(DB, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD) // specify dat
 			console.log(`  - API_HOST: [${HOST}]`);
 			console.log(`  - API_PORT: [${PORT}]`);
 			console.log(`  - API_STATIC_DIR: [${STATIC_DIR}]`);
-			console.log(`  + DB_ENV for ${db.namespace}:`);
-			console.log(`    - DB: [${db.databaseName}]`);
-			console.log(`    - DB_HOST: [${DB_HOST}]`);
-			console.log(`    - DB_PORT: [${DB_PORT}]`);
-			console.log(`    - DB_USERNAME: [${DB_USERNAME}]`);
-			console.log(`    - DB_PASSWORD: [${DB_PASSWORD}]`);
+			console.log(`  + DB ENV for ${db.namespace}:`);
+			console.log(`    - API_DB: [${db.databaseName}]`);
+			console.log(`    - API_DB_HOST: [${DB_HOST}]`);
+			console.log(`    - API_DB_PORT: [${DB_PORT}]`);
+			console.log(`    - API_DB_USERNAME: [${DB_USERNAME}]`);
+			console.log(`    - API_DB_PASSWORD: [${DB_PASSWORD}]`);
 		});
 	}) // if something gets mangled, we'll arive here
 	.catch((error: Error) => console.log(error));
