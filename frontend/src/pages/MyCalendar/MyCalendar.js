fae2b0ea654f83b1023d9f7b36efa7efc1ffd3aa
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-03-03 00:01:12 -0600

    Update nav

--- frontend/src/pages/MyCalendar/MyCalendar.js
+++ frontend/src/pages/MyCalendar/MyCalendar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from '../../components/Nav'
 import Calendar from './componets/Calendar';
 import '../../styles/pages/MyCalendar/MyCalendar.css';
 
 function WorkoutCalendar(){
     return(
         <div className="my-calendar-page">
+            <Nav/>
             <Calendar/>
         </div>
     );
