282f34509db598e6022a8c53cf8a759b33e4b4ff
Diego Suarez <diego@diegos-mbp-2.attlocal.net>
2023-03-04 00:58:36 -0600

    no capitals

--- api/src/models/splits.ts
+++ api/src/models/splits.ts
@@ -2,6 +2,11 @@ import { ObjectId } from 'mongodb';
 
 export default class Split {
     constructor(
-        public temp: string
+        public sets: number,
+        public reps: number,
+        public notes: Array<string>, // in case we have more than one note per split 
+        public exercise_List: Array<string>,
+        public target_Areas?: Array<string>
+
     ){}
 }
