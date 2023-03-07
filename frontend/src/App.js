c64680e82aa8d9d4617c3157f1c7d08c07a895cb
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-03-07 00:45:22 -0600

    Add Error 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 import UserDashboard from './pages/UserDashboard/UserDashboard';
 import MyCalendar from './pages/MyCalendar/MyCalendar';
+import Error404 from './pages/Error404/Error404';
 import './styles/App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/dashboard" element={<UserDashboard/>}/>
         <Route path="/my-calendar" element={<MyCalendar/>}/>
+        <Route path="/*" element={<Error404/>}/>
       </Routes>
     </Router>
   );
