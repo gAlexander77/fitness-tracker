04d944e35a2100d99bbe76ec7f934fbe56a45237
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-03-07 15:53:59 -0600

    Start bmi calculator

---
+++ frontend/src/pages/BodyMassIndexCalculator/BodyMassIndexCalculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../../components/Nav';
+import '../../styles/pages/BodyMassIndexCalculator/BodyMassIndexCalculator.css';
+
+function BodyMassIndexCalculator(){
+    return(
+        <div className="body-mass-index-calculator-page">
+            <Nav/>
+            BMI Calculator
+        </div>
+    )
+}
+
+export default BodyMassIndexCalculator;
