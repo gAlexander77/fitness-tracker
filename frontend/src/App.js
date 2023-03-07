a162684cc29a74f6962647fef6f1a24beb7e8577
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-03-07 15:53:59 -0600

    Start bmi calculator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import UserDashboard from './pages/UserDashboard/UserDashboard';
 import MyJournal from './pages/MyJournal/MyJournal';
 import MyCalendar from './pages/MyCalendar/MyCalendar';
 import Calculators from './pages/Calculators/Calculators';
+import BodyMassIndexCalculator from './pages/BodyMassIndexCalculator/BodyMassIndexCalculator';
 import Error404 from './pages/Error404/Error404';
 import './styles/App.css';
 
@@ -21,6 +22,7 @@ function App() {
         <Route path="/my-journal" element={<MyJournal/>}/>
         <Route path="/my-calendar" element={<MyCalendar/>}/>
         <Route path="/calculators" element={<Calculators/>}/>
+        <Route path="calculator/body-mass-index" element={<BodyMassIndexCalculator/>}/>
         <Route path="/*" element={<Error404/>}/>
       </Routes>
     </Router>
