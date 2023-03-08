3516e9eca307c1e3753dcea83b273197ea026b65
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-03-08 00:21:15 -0600

    Add test data for journal

--- frontend/src/pages/MyJournal/MyJournal.js
+++ frontend/src/pages/MyJournal/MyJournal.js
@@ -4,13 +4,15 @@ import TodaysEntity from './components/TodaysEntry';
 import ViewJournalEntries from './components/ViewJournalEntries';
 import '../../styles/pages/MyJournal/MyJournal.css';
 
+import journalRequestData from './testData/testData';
+
 function Journal(){
     return(
         <div className="my-journal-page">
             <Nav/>
             <h1 id="page-header">MY JOURNAL</h1>
-            <TodaysEntity/>
-            <ViewJournalEntries/>
+            <TodaysEntity data={journalRequestData}/>
+            <ViewJournalEntries data={journalRequestData}/>
         </div>
     );
 }
