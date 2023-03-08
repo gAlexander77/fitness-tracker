5d68cfa51e5d4d6fa855af4093ae1d5478c79eb1
BarkerProoks <jonparkerbrooks@gmail.com>
2023-03-07 19:39:50 -0600

    changed object to hash map

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
+const methods =new Map([
+    ["GET", "\x1b[32mGET \x1b[0m",],
+    ["POST", "\x1b[35mPOST\x1b[0m",],
+    ["DELETE", "\x1b[31mDEL \x1b[0m"]
+]);
 
 export const log = {
     ok: (message: string) => console.log(`+ ok: ${message}`),
@@ -7,12 +12,11 @@ export const log = {
 };
 
 export const httpLog = async (req: Request, res: Response, next: NextFunction) => {
-    res.on("finish", () => {
-        const method = req.method === "GET" ? "\x1b[32mGET \x1b[0m" : "\x1b[35mPOST\x1b[0m";
+    res.on("finish", () => { 
         const status = res.statusCode;
         const date = `\x1b[34m${new Date().toLocaleString()}\x1b[0m`;
         const code = `\x1b[3${Math.floor(status / 100)}m${status}\x1b[0m`;
-        console.log(`[${date}]: ${method} - ${code}, ${res.statusMessage} - ${req.originalUrl}`);
+        console.log(`[${date}]: ${methods.get(req.method)} - ${code}, ${res.statusMessage} - ${req.originalUrl}`);
     });
     next();
 }
