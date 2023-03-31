9dcfd800762922cc9167ef066c215419f9e7f0b4
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-03-31 12:06:06 -0500

    Add search and fix page pagination

--- frontend/src/pages/Workouts/Workouts.js
+++ frontend/src/pages/Workouts/Workouts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../../components/Nav';
 import Search from './components/Search';
 import Display from './components/Display';
@@ -9,14 +9,32 @@ import workoutData from '../../test-data/workoutsRequest.json';
 function Workouts(){
     
     const data = workoutData;
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const [searchQuery, setSearchQuery] = useState("");
+    
+    const filteredWorkouts = data.filter((workout) =>
+        workout.workoutName.toLowerCase().replace(/-/g, " ").includes(searchQuery.toLowerCase().replace(/-/g, " "))
+    );
+    
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+        setCurrentPage(1);
+    };
+
     console.log(data);
 
     return(
         <div className="workouts-page">
             <Nav/>
             <h1 id="page-title">WORKOUTS</h1>
-            <Search/>
-            <Display data={data}/>
+            <Search handleSearchChange={handleSearchChange}/>
+            <Display 
+                data={filteredWorkouts}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
         </div>
     );
 }
--- frontend/src/pages/Workouts/components/Display.js
+++ frontend/src/pages/Workouts/components/Display.js
@@ -3,42 +3,86 @@ import { useNavigate } from 'react-router-dom';
 
 function Display(props) {
     
-    const [currentPage, setCurrentPage] = useState(1);
     const workoutsPerPage = 12;
     const numWorkouts = props.data.length;
     const numPages = Math.ceil(numWorkouts / workoutsPerPage);
-    const startIdx = (currentPage - 1) * workoutsPerPage;
+    const startIdx = (props.currentPage - 1) * workoutsPerPage;
     const endIdx = Math.min(startIdx + workoutsPerPage, numWorkouts);
-
+  
     const handlePageClick = (pageNum) => {
-        setCurrentPage(pageNum);
-    }
-
+      props.setCurrentPage(pageNum);
+    };
+  
+    const renderPaginationItems = () => {
+        const items = [];
+        const maxVisiblePages = 5;
+        const sidePages = Math.floor(maxVisiblePages/2);
+    
+        items.push(
+            <button
+                key={1}
+                onClick={() => handlePageClick(1)}
+                id={1 === props.currentPage ? "active" : ""}
+            >
+                1
+            </button>
+        );
+    
+        if(props.currentPage > sidePages + 2) {
+            items.push(<span key="ellipsis-start">...</span>);
+        }
+    
+        for(
+            let pageNum = Math.max(2, props.currentPage - sidePages);
+            pageNum <= Math.min(numPages - 1, props.currentPage + sidePages);
+            pageNum++
+        ) {
+            const isActive = pageNum === props.currentPage;
+            items.push(
+                <button
+                    key={pageNum}
+                    onClick={() => handlePageClick(pageNum)}
+                    id={isActive ? "active" : ""}
+                >
+                    {pageNum}
+                </button>
+            );
+        }
+    
+        if (props.currentPage < numPages - sidePages - 1) {
+            items.push(<span key="ellipsis-end">...</span>);
+        }
+    
+        items.push(
+            <button
+                key={numPages}
+                onClick={() => handlePageClick(numPages)}
+                id={numPages === props.currentPage ? "active" : ""}
+            >
+                {numPages}
+            </button>
+        );
+    
+        return items;
+      };    
+  
     return (
-        <div className='workouts-display-component'>
-            <div className="workouts-display-workouts-container ">
-                {props.data.slice(startIdx, endIdx).map((workout, index)=>{
-                    return(
-                        <Workout
-                            key={index}
-                            name={workout.workoutName}
-                            images={workout.images}
-                        />
-                    );
-                })}
-            </div>
-            <div className="workouts-display-pagination">
-                {Array.from({length: numPages}, (_, i) => i + 1).map(pageNum => {
-                    const isActive = pageNum === currentPage;
-                    return (
-                        <button key={pageNum} onClick={() => handlePageClick(pageNum)}
-                            id={isActive ? 'active' : ''}>{pageNum}</button>
-                    );
-                })}
-            </div>
+      <div className="workouts-display-component">
+        <div className="workouts-display-workouts-container">
+          {props.data.slice(startIdx, endIdx).map((workout, index) => {
+            return (
+              <Workout
+                key={index}
+                name={workout.workoutName}
+                images={workout.images}
+              />
+            );
+          })}
         </div>
+        <div className="workouts-display-pagination">{renderPaginationItems()}</div>
+      </div>
     );
-}
+  }
 
 function Workout(props) {
     
