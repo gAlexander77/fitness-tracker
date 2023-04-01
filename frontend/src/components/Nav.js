efa257d0a9693158fd83f72ca1102b8b75d7022c
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-04-01 17:53:24 -0500

    Temporary fix

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,46 +4,46 @@ import { FaUserCircle } from 'react-icons/fa';
 import '../styles/components/Nav.css';
 
 function Nav() {
-  const navigate = useRef(useNavigate());
+    const navigate = useRef(useNavigate())
 
-  const goToHome = () => {
-    navigate.current('/');
-  };
+    const goToHome = () => {
+	    navigate.current('/');
+    };
 
-  const goToDashboard = () => {
-    navigate.current('/dashboard');
-  };
+    const goToDashboard = () => {
+        navigate.current('/dashboard');
+    };
 
-  return (
+return (
     <nav className="nav">
-      <div className="nav-left">
-        <button className="nav-logo-link" onClick={goToHome}>
-          <h1>SHAPE</h1>
-          <h1>SHIFT</h1>
-        </button>
-        <div className="hamburger-menu" onClick={() => {
-          document.querySelector('.hamburger-menu').classList.toggle('active');
-          document.querySelector('.menu').classList.toggle('active');
-        }}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div className="nav-right">
-        <button className="nav-dashboard-link" onClick={goToDashboard}>
-          <p>Dashboard</p>
-          <FaUserCircle id="icon" />
-        </button>
-      </div>
-      <div className="menu">
-        <ul>
-          <li>Option 1</li>
-          <li>Option 2</li>
-          <li>Option 3</li>
-        </ul>
-      </div>
-    </nav>
+	    <div className="nav-left">
+            <button className="nav-logo-link" onClick={goToHome}>
+                <h1>SHAPE</h1>
+                <h1>SHIFT</h1>
+            </button>
+	    </div>
+	    <div className="nav-right">
+	        <button className="nav-dashboard-link" onClick={goToDashboard}>
+		        <p>Dashboard</p>
+		        <FaUserCircle id="icon"/>
+	        </button>
+		    <div className="hamburger-menu" onClick={() => {
+			    document.querySelector('.hamburger-menu').classList.toggle('active');
+			    document.querySelector('.nav-menu').classList.toggle('active');
+		    }}>
+		        <span></span>
+			    <span></span>
+			    <span></span>
+	        </div>
+            <div className="nav-menu">
+                <ul>
+                    <li>Option 1</li>
+                    <li>Option 2</li>
+                    <li>Option 3</li>
+                </ul>
+		    </div>
+	    </div>
+	</nav>
   );
 }
 
