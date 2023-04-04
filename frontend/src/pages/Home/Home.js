1f2ab534a1cd2bb5d07c013e83387a82a7559d08
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-04-04 01:07:05 -0500

    Add test button for demo

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HomeWelcome from './components/HomeWelcome';
 import HomeMenuOptions from './components/HomeMenuOptions';
 import HomeAccount from './components/HomeAccount';
 import '../../styles/pages/Home/Home.css';
 
 function Home(){
-    const isUser = true;
+    const [isUser, setIsUser] = useState(true);
     return(
         <div className="home-page">
             <HomeAccount isUser={isUser}/>
             <div className="home-body-container">
                 <HomeWelcome isUser={isUser}/>
                 <HomeMenuOptions isUser={isUser}/>
+                <TestButton isUser={isUser} setIsUser={setIsUser}/>
             </div>
         </div>
+    );
+}
+
+function TestButton(props) {
+    return(
+        <button 
+            onClick={() => props.setIsUser(!props.isUser)}
+            style={{
+                all: 'unset',
+                backgroundColor: 'transparent',
+                width: '100px',
+                height: '100px',
+                position: 'absolute',
+                bottom: 0,
+                right: 0,
+            }}
+        />
     );
 }
 
