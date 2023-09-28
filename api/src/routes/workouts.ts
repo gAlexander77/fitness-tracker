513b2d0205ddec0262626c503150b29be946542e
Jeremy Groce <108232216+JJGroce95@users.noreply.github.com>
2023-09-28 00:15:29 -0500

    Update workouts.ts
    Allowed creation of new workouts (exercises).  All changes are within comments.

--- api/src/routes/workouts.ts
+++ api/src/routes/workouts.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import { collections } from '../db';
 
 const workouts = Router();
+// const userWorkouts = async (id: ObjectId) => (await collections.users.fineOne({ _id: id})).workoutName
 
 workouts.get("/", async (_req: Request, res: Response) => {
     try {
@@ -23,5 +24,28 @@ workouts.get("/:name", async (req: Request, res: Response) => {
 		res.status(400).json({ error: "invalid request" });
 	}
 });
+
+/*
+workouts.post("/create", async (req: Request, res: Response) => {
+    try{
+        //get user session ID
+        const id = new ObjectID(req.session._id);
+        const workoutName = await userWorkouts(id);
+
+        workoutName.forEach((Workout: Workout) => {
+            if (Workout.workoutName === req.body.workoutName)
+                throw new Error("group already exists");
+        });
+        //create a new class for a new workout to be added
+        const workoutName = new Workout(req.body.workoutName);
+        const updated = await collections.users.updateOne({ _id: id}, { $push: { workoutName: Workout}});
+        updated.modifiedCount > 0
+            ? res.status(200).json({ ok: "added workout"})
+            : res.status(500).json({ error: "internal server error"});
+    } catch(error) {
+        res.status(400).json({error: error});
+    }
+    });
+*/
 
 export default workouts;
