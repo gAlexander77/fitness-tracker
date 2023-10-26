19e4c1ff361ab5e9481be235bb25f7dfcb724826
Jeremy Groce <108232216+JeremyGroce@users.noreply.github.com>
2023-10-26 11:25:38 -0500

    Update ProteinIntakeCalculator.js
    Reworked it from scratch. Needs to be finished.

--- frontend/src/pages/ProteinIntakeCalculator/ProteinIntakeCalculator.js
+++ frontend/src/pages/ProteinIntakeCalculator/ProteinIntakeCalculator.js
@@ -2,196 +2,182 @@ import React, { useState } from 'react';
 import Nav from '../../components/Nav';
 import '../../styles/pages/ProteinIntakeCalculator/ProteinIntakeCalculator.css';
 
+import { heightToInches, calculateProteinIntake} from './utils';
 
-// To Do: age, sex
+// Todo: make results pop up on click
 
-// input - radio button
-// sex - radio button
-//Pick a reputable source to use as a calculator
-import {heightToInches, calculateProtein} from './utils';
+function ProteinIntakeCalculator() {
+    const [showResults, setShowResults] = useState(false);
+    const [input, setInput] = useState({
+      weight: 0,
+      gender: 'male',
+      age: 0,
+      feet: 0,
+      inches: 0,
+      activityLevel: 'sedentary',
+    });
+    const [results, setResults] = useState({
+      proteinIntake: 0,
+    });
+  
+    const activityLevels = ['sedentary', 'light', 'moderate', 'active', 'very active'];
+  
 
-function ProteinIntakeCalculator()
-{
-        const [showResults, setShowResults] = useState(false);
+  
+  
+    return (
+      <div className="protein-intake-calculator-page">
+        <Nav />
+        <h1>Protein Intake Calculator</h1>
+        <Calculator
+          input={input}
+          setInput={setInput}
+          results={results}
+          setResults={setResults}
+          activityLevels={activityLevels}
+          setShowResults={setShowResults}
 
-        // create initial values
-        const[input,setInput] = useState({
-          feet: 0,
-          inches: 0,
-          weight: 0,
-          activity: 0
-        });
-
-        // output results
-        const[results,setResults] = useState({
-          protein: 0,
-        })
-
-        return(
-          <div className = "protein-intake-calculator-page">
-            <h1 id = "title">PROTEIN CALCULATOR</h1>
-            <Calculator
-              input = {input}
-              setInput={setInput}
-              setResults={setResults}
-              results={results}
-              setShowResults={setShowResults}
-            />
-            <CalculatorResults
-              trigger={showResults}
-              setTrigger={setShowResults}
-              protein={results.protein}
-              />
-          </div>
-        );
-}
-
-function Calculator(props)
-{
-  const setValueHandler = (event) => {
-      const { id, value } = event.target;
-      switch (id) {
-        case "feet":
-          props.setInput((prev) => ({ ...prev, feet: value }));
-          break;
-        case "inches":
-          props.setInput((prev) => ({ ...prev, inches: value }));
-          break;
-        case "weight":
-          props.setInput((prev) => ({ ...prev, weight: value }));
-          break;
-          // Activity level (1,2,3)
-        case "activity":
-          props.setInput((prev) => ({ ...prev, weight: value }));
-          break; 
-        default:
-          break;
-      }
-  };
-
-  const calculateHandler = () => {
-    let height = heightToInches(props.input.feet, props.input.inches);
-    // get result
-    let protein = calculateProtein(height,props.input.weight, props.input.activity);
-    props.setResults({protein:protein});
-    // maybe
-    if(protein > 0)
-    {
-      props.setShowResults(true);
-    }
-    else
-    {
-      props.setShowResults(false);
-    }
+        />
+        <CalculatorResults
+          trigger={showResults}
+          setTiggers={setShowResults}
+          proteinIntake={results.proteinIntake}
+        />
+      </div>
+    );
   }
 
+  function Calculator(props) {
+    const setValueHandler = (event) => {
+        const { id, value } = event.target;
+        switch (id) {
+          case "feet":
+            props.setInput((prev) => ({ ...prev, feet: value }));
+            break;
+          case "inches":
+            props.setInput((prev) => ({ ...prev, inches: value }));
+            break;
+          case "weight":
+            props.setInput((prev) => ({ ...prev, weight: value }));
+            break;
+          case "gender":
+            props.setInput((prev) => ({ ...prev, gender: value }));
+            break;
+          case "age":
+            props.setInput((prev) => ({ ...prev, age: value }));
+            break;
+          case "activityLevel":
+            props.setInput((prev) => ({ ...prev, activityLevel: value }));
+            break;
+          default:
+            break;
+        }
+      };
 
-return(
-  <div className="protein-intake-calculator-calculator-component">
-      <div className="protein-intake-calculator-calculator-inner">
-        <div className="protein-intake-calculator-calculator-left-container">
-          {/* Get Height */}
-          <h1 id="title">Height</h1>
-          <div id="input-container">
-            {/* input feet */}
-            <input
-              id="feet"
-              type="number"
-              value={props.input.feet}
-              onChange={setValueHandler}
-            />
-            <p id="tag">ft</p>
-          </div>
-          <div id="input-container">
-            {/* input inches */}
-            <input
-              id="inches"
-              type="number"
-              value={props.input.inches}
-              onChange={setValueHandler}
-            />
-            <p id="tag">in</p>
-          </div>
-          {/* get weight */}
-          <h1 id="title">Weight</h1>
-          {/* input lbs */}
-          <div id="input-container">
-            <input
-              id="weight"
-              type="number"
-              value={props.input.weight}
-              onChange={setValueHandler}
-            />
-            <p id="tag">lbs</p>
-          </div>
-        </div>
-        
-        {/* Get Activity Levels (drop down) */}
+      const calculateHandler = () => {
+        // let height = heightToInches(props.input.feet, props.input.inches);
+        // console.log(height);
+        let proteinIntake = 1;
 
-        <div className="protein-intake-calculator-calculator-right-container">
-          <h1 id="title">Activity Level </h1>
-          <div id="input-container">
-            <div className="protein-intake-calclator-calculator-drop-down">
-            <select name = "activity" id="activity"
-
-// Problem here:
-            // value={props.input.weight}
-            //   onChange={setValueHandler}
-            >
-              <option value={1}>
-                sedentary: little/no exercise
-              </option>
-              <option value={2}>
-                light: exercise 1-3x/week
-              </option>
-              <option value={3}>
-                moderate: exercise 4-5x/week
-              </option>
-              <option value={4}>
-                active: exercise daily
-              </option>
-              <option value={5}>
-                very active: intense daily exercise
-              </option>
-            </select>
-            <div className="protein-intake-calculator-activity-description">
-              <br></br>
-              <p><b>Exercise:</b><br></br> 15-30 min elevated heart rate </p>
-              <p>
-                <b>Intense exercise:</b> <br></br>45-12 min elevated heart rate
-              </p>
-              <p>
-                <b>Very intense exercise:</b><br></br> 2+ hrs elevated heart rate 
-
-              </p>
-            </div>
-            </div>
-          </div>
-
-
-      </div>
-
+        props.setResults({ proteinIntake: proteinIntake });
+        props.setShowResults(true);
+    }
       
-    </div>
-    <button className="protein-intake-calculator-result-btn" onClick={calculateHandler}>
-    Calculate
-    </button>
-    </div>
-  );
-}
+    return (
+        <div className="protein-intake-calculator-calculator-component">
+          <div className="protein-intake-calculator-calculator-inner">
+            <div className="protein-intake-calculator-calculator-left-container">
+              <h1 id="title">Height</h1>
+              <div id="input-container">
+                <input
+                  id="feet"
+                  type="number"
+                  value={props.input.feet}
+                  onChange={setValueHandler}
+                />
+                <p id="tag">Feet</p>
+              </div>
+              <div id="input-container">
+                <input
+                  id="inches"
+                  type="number"
+                  value={props.input.inches}
+                  onChange={setValueHandler}
+                />
+                <p id="tag">Inches</p>
+              </div>
+              <h1 id="title">Age</h1>
+              <div id="input-container">
+                <input
+                  id="age"
+                  type="number"
+                  value={props.input.age}
+                  onChange={setValueHandler}
+                />
+                <p id="tag">years</p>
+
+              </div>
+            </div>
+            <div className="protein-intake-calculator-calculator-right-container">
+              <h1 id="title">Weight</h1>
+              <div id="input-container">
+                <input
+                  id="weight"
+                  type="number"
+                  value={props.input.weight}
+                  onChange={setValueHandler}
+                />
+                <p id="tag">lbs</p>
+              </div>
+              <div className="input-container">
+                <h1 id="title">Gender</h1>
+                <select
+                  id="gender"
+                  value={props.input.gender}
+                  onChange={setValueHandler}
+                >
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+              <div className="input-container">
+                <h1 id="title">Activity Level</h1>
+                <select
+                  id="activityLevel"
+                  value={props.input.activityLevel}
+                  onChange={setValueHandler}
+                >
+                  {props.activityLevels.map((level) => (
+                    <option key={level} value={level}>
+                      {level}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+          <button className="protein-intake-calculator-calculator-calculate-btn" onClick={props.calculateHandler}>
+            Calculate
+          </button>
+        </div>
+      );
+      
+  }
 
 function CalculatorResults(props) {
-  return props.trigger ? (
-    <div className="protein-intake-calculator-calculator-results-component">
-      <div id="inner">
-        <div id="display-container">
-          <h1>Protein Intake</h1>
-          <h1>{props.protein} g/day</h1>
+    
+    return(props.trigger)?(
+        <div className="protein-intake-calculator-calculator-results-component">
+        <div id="inner">
+            <div id="display-container">
+            <h1>Protein Intake</h1>
+            <h1>{props.proteinIntake} grams</h1>
+            </div>
         </div>
-      </div>
-      {true ? <button>Save results to Journal</button> : ""}
-    </div>
-  ) : "";
+        {true ? <button>Save results to Journal</button> : ""}
+        </div>
+    ):'';
 }
-
-export default ProteinIntakeCalculator;
+  
+  
+  export default ProteinIntakeCalculator;
