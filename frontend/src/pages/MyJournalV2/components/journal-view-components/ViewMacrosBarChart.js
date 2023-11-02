eff0c9171c22f8b935e11a06dd1f574e1d8ac1d4
Diego Suarez <77934556+DiegoS24@users.noreply.github.com>
2023-11-02 14:43:04 -0500

    temporary styling
    temporary styling for all graphs in Journal

--- frontend/src/pages/MyJournalV2/components/journal-view-components/ViewMacrosBarChart.js
+++ frontend/src/pages/MyJournalV2/components/journal-view-components/ViewMacrosBarChart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Bar } from "react-chartjs-2";
-import {Chart as ChartJS } from "chart.js/auto"
+import { Chart as ChartJS } from "chart.js/auto"
+import '../../../../styles/pages/MyJournalV2/components/journal-view-components/ViewMacrosBarChart.css';
 
 export default function ViewMacrosBarChart({macrosData}) {
 
@@ -30,7 +31,11 @@ export default function ViewMacrosBarChart({macrosData}) {
                 labels: labels,
                 datasets: [{
                     label: "grams",
-                    data: data
+                    data: data,
+                    backgroundColor: 'rgba(254, 62, 45, 0.6)',
+                    borderColor: '#FE3E2D',
+                    borderWidth: 1,
+                    borderRadius: 10,
                 }]
             });
             setShowChart(true);
@@ -43,18 +48,31 @@ export default function ViewMacrosBarChart({macrosData}) {
     const options = {
         scales: {
             x: {  // No need to specify 'type' for x-axis. It's inferred as 'category'.
-                beginAtZero: true
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        size: 16
+                    }
+                }
             },
             y: {
                 type: 'linear',
-                beginAtZero: true
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        size: 16
+                    }
+                }
             }
         }
     };
 
     return(showChart) ? (
-        <div style={{ width: '100%', height: '200px'}}>
-            <Bar data={chartData}  options={options}/>
+        <div className='container'>
+            <div style={{ width: '500px', height: '300px'}}>
+                <Bar data={chartData}  options={options}/>
+            </div>
         </div>
+        
     ): '';
 }
--- frontend/src/pages/UserDashboardV2/dysc-components/macroswidget.js
+++ frontend/src/pages/UserDashboardV2/dysc-components/macroswidget.js
@@ -12,7 +12,7 @@ function MacrosWidget() {
     macros = MacrosIDHelper(data.journalEntries);
 
     return (
-        <div>
+        <div class='graphs-container'>
             <div className='Activity-Ring'>
                 <DonutChart id="User-Carbs-Intake" dailyIntake={macros[2]} dailyTotal={100} macro={"g Carbs"} color={"#2DEDF9"}></DonutChart>
                 <DonutChart id="User-Calorie-Intake" dailyIntake={ macros[0] } dailyTotal={1000} macro={"Calories"} color={"#6AFF00"}></DonutChart>
