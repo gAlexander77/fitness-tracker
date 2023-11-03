743d526b9c58a7dc690e48e508f55ae140de48ee
Jeremy Groce <108232216+JeremyGroce@users.noreply.github.com>
2023-11-03 17:24:25 -0500

    Update BodyFatPercentageCalculator.js

--- frontend/src/pages/BodyFatPercentageCalculator/BodyFatPercentageCalculator.js
+++ frontend/src/pages/BodyFatPercentageCalculator/BodyFatPercentageCalculator.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import Nav from '../../components/Nav';
-import '../../styles/pages/BodyFatPercentageCalculator/BodyFatPercentageCalculator.css';
-
-function BodyFatPercentageCalculator(){
-    return(
-        <div className="body-fat-percentage-calculator-page">
-            <Nav/>
-            <h1>Body Fat Percentage Calculator</h1>
-        </div>
-    );
+export function heightToInches(feet, inches){
+    feet = parseInt(feet);
+    inches = parseInt(inches);
+    if(feet >= 0 && inches >= 0) {
+        return ((feet*12)+inches);
+    }    
+    else {
+        return -1;
+    } 
 }
 
-export default BodyFatPercentageCalculator;
+export function calculateBodyFatPercentage(height, gender, neck, waist, hip, age) {
+    let bodyFatPercentage;
+    // men
+    
+    if (gender === 5) {
+        bodyFatPercentage = 86.010 * Math.log10(waist - neck) - 70.041 * Math.log10(height) + 36.76;
+    } 
+    // women
+    else if (gender === -161) {
+        console.log(hip);
+
+        bodyFatPercentage = 163.205 * Math.log10(waist - (-hip) - neck) - 97.684 * Math.log10(height) - 78.387;
+        console.log(bodyFatPercentage);
+
+    } 
+  
+    return bodyFatPercentage.toFixed(1);
+}
+  
