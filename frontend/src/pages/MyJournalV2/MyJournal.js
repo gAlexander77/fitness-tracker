953a9c0dadfe371bc76285de0b019ff38061b2dc
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-11-05 19:54:54 -0600

    add journal reload

--- frontend/src/pages/MyJournalV2/MyJournal.js
+++ frontend/src/pages/MyJournalV2/MyJournal.js
@@ -1,71 +1,77 @@
-import React, { useState } from 'react';
-import { BsGraphUp, BsFillJournalBookmarkFill } from 'react-icons/bs';
+import React, { useState, useEffect } from "react";
+import { BsGraphUp, BsFillJournalBookmarkFill } from "react-icons/bs";
 // Default Imports
-import Nav from '../../components/Nav'
-import Background from '../../components/Background';
-import Footer from '../../components/Footer';
+import Nav from "../../components/Nav";
+import Background from "../../components/Background";
+import Footer from "../../components/Footer";
 // Page specific imports
-import Journal from './components/Journal';
-import Graphs from './components/JournalData';
-import '../../styles/pages/MyJournalV2/MyJournal.css';
+import Journal from "./components/Journal";
+import Graphs from "./components/JournalData";
+import "../../styles/pages/MyJournalV2/MyJournal.css";
 
 // Test Data
-import data from '../../test-data/journalRequest.json';
+import data from "../../test-data/journalRequest.json";
 
 function MyJournal() {
-
     // Replace with data from GET /journal-entries
     let journalEntries = data.journalEntries;
+    const [toggle, setToggle] = useState(false);
+    // Reload Function to pass to other components
+    const reloadJournal = () => {
+        setToggle(!toggle);
+    };
+    useEffect(() => {
+        // GET request here for journal entries
+        //
+        //
+        //
+        console.log(toggle);
+    }, [toggle]);
 
     const [viewJournal, setViewJournal] = useState(true);
 
     return (
         <>
-            <Nav/>
+            <Nav />
             <div className="my-journal-page">
                 <div className="my-journal-content">
                     <div className="my-journal-dashboard">
                         <div className="header">
                             <h1 className="title">My Journal</h1>
-                            <HeaderToggle 
+                            <HeaderToggle
                                 viewJournal={viewJournal}
                                 setViewJournal={setViewJournal}
                             />
                         </div>
                         <div className="content">
-                            {viewJournal ? 
-                                <Journal 
+                            {viewJournal ? (
+                                <Journal
                                     journalEntries={journalEntries}
+                                    reloadJournal={reloadJournal}
                                 />
-                                : 
-                                <Graphs 
-                                    journalEntries={journalEntries}
-                                />
-                            }
+                            ) : (
+                                <Graphs journalEntries={journalEntries} />
+                            )}
                         </div>
                     </div>
                 </div>
-                <Background/>
+                <Background />
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 }
 
-function HeaderToggle({viewJournal, setViewJournal}) {
-    
+function HeaderToggle({ viewJournal, setViewJournal }) {
     const toggle = () => {
         setViewJournal(!viewJournal);
-    }
-    
-    return (viewJournal ? 
-        <BsGraphUp 
-            onClick={toggle} 
-            className="journal-toggle-btn"
-        />
-        : 
-        <BsFillJournalBookmarkFill 
-            onClick={toggle} 
+    };
+
+    return viewJournal ? (
+        <BsGraphUp onClick={toggle} className="journal-toggle-btn" />
+    ) : (
+        <BsFillJournalBookmarkFill
+            onClick={toggle}
             className="journal-toggle-btn"
         />
     );
